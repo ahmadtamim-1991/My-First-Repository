c02fbc25ef3c0ac35530afa1b2f14a0259b0a688
Your Name <you@example.com>
2019-12-02 18:16:09 -0500

    add a new sysout

--- src/main/java/AppControllers/jjjjjjjjjjjjjjpoeriewrpoweriw.java
+++ src/main/java/AppControllers/jjjjjjjjjjjjjjpoeriewrpoweriw.java
@@ -4,5 +4,6 @@ public class jjjjjjjjjjjjjjpoeriewrpoweriw {
 
 	public void showchange() {
 		System.out.println("hasib jaan da konet toffffffff konoommmmmmmmm");
+		System.out.println("mama samim da kone to ham k khafa nashi");
 	}
 }
